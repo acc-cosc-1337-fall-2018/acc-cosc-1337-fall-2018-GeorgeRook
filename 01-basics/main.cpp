c5898cd6fd021e6435e508216560ed735325bfe0
GeorgeRook <42852804+GeorgeRook@users.noreply.github.com>
2018-09-13 19:11:40 -0500

    Update main.cpp

--- 01-basics/main.cpp
+++ 01-basics/main.cpp
@@ -9,3 +9,24 @@ int main()
 
 	return 0;
 }
+
+#include "references.h"
+#include<iostream>
+
+using std::cout;
+
+int main()
+{
+	int num1{ 0 };
+	int num2{ 0 };
+
+	cout << "before:" << num1 << " " << num2<<'\n';
+
+	pass_by_val_by_ref(num1, num2);
+
+	cout << "after: " << num1 << " " << num2;
+
+
+
+	return 0;
+}
